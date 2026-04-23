c383b1585ce55eac61d7f078cd873dba4f593a7d
Teodora <tedy.aa@abv.bg>
2026-04-23 11:54:10 +0200

    added more functionality to todos

--- server/index.js
+++ server/index.js
@@ -8,18 +8,58 @@ app.use(express.json())
 let todos = []
 let id = 1
 
+
 app.get('/todos', (req, res) => {
   res.json(todos)
 })
 
+
 app.post('/todos', (req, res) => {
   const newTodo = {
     id: id++,
-    text: req.body.text
+    name: req.body.name,
+    description: req.body.description,
+    priority: req.body.priority,
+    color: req.body.color,
+    date: req.body.date,
+    completed: false
   }
+
   todos.push(newTodo)
   res.json(newTodo)
 })
+
+app.put('/todos/:id', (req, res) => {
+  const todoId = Number(req.params.id)
+
+  todos = todos.map(todo =>
+    todo.id === todoId
+      ? { ...todo, completed: !todo.completed }
+      : todo
+  )
+
+  res.json({ message: 'Updated' })
+})
+
+app.put('/todos/:id/edit', (req, res) => {
+  const todoId = Number(req.params.id)
+
+  todos = todos.map(todo =>
+    todo.id === todoId
+      ? {
+          ...todo,
+          name: req.body.name,
+          description: req.body.description,
+          priority: req.body.priority,
+          color: req.body.color,
+          date: req.body.date
+        }
+      : todo
+  )
+
+  res.json({ message: 'Todo updated' })
+})
+
 
 app.delete('/todos/:id', (req, res) => {
   const todoId = Number(req.params.id)
